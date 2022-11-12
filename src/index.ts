1f1c6ab3a052e10acd8876c7df723f11e739c29a
sonyakim-dev <sonyakim.dev@gmail.com>
2022-11-12 01:50:30 -0800

    finish project

--- src/index.ts
+++ src/index.ts
@@ -20,3 +20,29 @@ class Block implements BlcokShape {
     return crypto.createHash("sha256").update(toHash).digest("hex")
   }
 }
+
+class Blockchain {
+  private blocks: Block[]
+  constructor() {
+    this.blocks = []
+  }
+  private getPrevHash() {
+    if (this.blocks.length === 0) return ''
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+  public addBlock(data: string) {
+    const newBlock = new Block(this.getPrevHash(), this.blocks.length + 1, data);
+    this.blocks.push(newBlock)
+  }
+  public getBlocks() {
+    return [...this.blocks]; // prevente accesting private member
+  }
+}
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock("First");
+blockchain.addBlock("Second");
+blockchain.addBlock("Third");
+
+console.log(blockchain.getBlocks());
